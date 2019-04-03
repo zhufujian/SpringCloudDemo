1be32f32789401aa119dae04de29cb123da2c74f
zhufujian <zhu_fujian@163.com>
2019-04-03 09:25:48 +0800

    使用配置文件引用Ribbon
    使用配置文件引用Ribbon

--- eureka-consumer/src/main/java/com/study/demo/controler/MyLoadBalanceClient.java
+++ eureka-consumer/src/main/java/com/study/demo/controler/MyLoadBalanceClient.java
@@ -2,7 +2,7 @@ package com.study.demo.controler;
 
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
-@RibbonClient(name = "eureka-provider", configuration = MyConfig.class)
+//@RibbonClient(name = "eureka-provider", configuration = MyConfig.class)
 public class MyLoadBalanceClient {
 
 }
